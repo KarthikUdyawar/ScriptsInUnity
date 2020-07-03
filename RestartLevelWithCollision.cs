49166843f4c0e6270315f7f24fa97f4052202630
Karthik Udyawar <54676225+KarthikUdyawar@users.noreply.github.com>
2020-07-03 23:38:42 +0530

    Create RestartLevelWithCollision.cs

---
+++ RestartLevelWithCollision.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLevelWithCollision : MonoBehaviour
+{
+    [SerializeField]
+    string strTag;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == strTag)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
