dc022ca53de88d076fcafae573aa6c86c6f18b14
Karthik Udyawar <54676225+KarthikUdyawar@users.noreply.github.com>
2020-07-03 23:33:56 +0530

    Create LoadLevelOnCollision.cs

---
+++ LoadLevelOnCollision.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelOnCollision : MonoBehaviour
+{
+    [SerializeField]
+    string strTag;
+
+    [SerializeField]
+    string strSceneName;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == strTag)
+            SceneManager.LoadScene(strSceneName);
+    }
+
+}
