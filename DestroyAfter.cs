15cc4380ff7b4e7fa009e6c7b5ea9f49c3a4bd0c
Karthik Udyawar <54676225+KarthikUdyawar@users.noreply.github.com>
2020-07-03 23:32:08 +0530

    Create DestroyAfter.cs

---
+++ DestroyAfter.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfter : MonoBehaviour
+{
+    [SerializeField]
+    float fTimer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        fTimer -= Time.deltaTime;
+        if (fTimer <= 0)
+              Destroy(this.gameObject); 
+    }
+}
